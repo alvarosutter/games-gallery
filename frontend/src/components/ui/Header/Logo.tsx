1854ed6873b34f969c0b6e66371b28543d593870
Alvaro Sutter <asutter.dev@gmail.com>
2024-05-29 17:04:49 +0200

    fix:lint setup

--- frontend/src/components/ui/Header/Logo.tsx
+++ frontend/src/components/ui/Header/Logo.tsx
@@ -3,7 +3,8 @@ import Link from 'next/link';
 export default function Logo() {
   return (
     <Link aria-label="Go to home page" href="/" className="">
-      <span className="text-blue-500">GAMES</span>GALLERY
+      <span className="text-blue-500">GAMES</span>
+      GALLERY
     </Link>
   );
 }
