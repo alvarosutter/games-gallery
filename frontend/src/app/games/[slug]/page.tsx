de47d502da90db92cf61cff08707b8f321dbe37f
Alvaro Sutter <asutter.dev@gmail.com>
2024-06-01 19:08:44 +0200

    feat: add memory game

--- frontend/src/app/games/[slug]/page.tsx
+++ frontend/src/app/games/[slug]/page.tsx
@@ -5,18 +5,24 @@ import { useState } from 'react';
 
 import DicePokerGame from '../../../components/games/DicePoker';
 import HigherLowerGame from '../../../components/games/HigherLower';
+import Memory from '../../../components/games/Memory';
 import PigGame from '../../../components/games/PigGame';
 import RockPaperScissors from '../../../components/games/RockPaperScissors';
 import Cover from '../../../components/ui/Game/Cover';
 import useLocalStorage from '../../../hooks/useLocalStorage';
 import type { Game } from '../../../types/game';
 import GamesData from '../../gamesData.json';
+import NotFound from '../../not-found';
 
 export default function Main() {
   const pathname = usePathname();
   const games = GamesData.games as Array<Game>;
   const game = games.filter((g) => g.href === pathname)[0];
   const [gameRunning, setGameRunning] = useState(false);
+
+  if (game === undefined) return <NotFound />;
+
+  // eslint-disable-next-line react-hooks/rules-of-hooks
   const { value: savedScore, setValue: setScore } = useLocalStorage(
     `${game.name}-score`,
     game.score
@@ -38,9 +44,9 @@ export default function Main() {
       case 'simongame':
         return <p>{name}</p>;
       case 'memory':
-        return <p>{name}</p>;
+        return <Memory game={game} score={savedScore} setScore={setScore} />;
       default:
-        return <p>{`GAME: ${name} NOT FOUND`}</p>;
+        return <NotFound />;
     }
   }
 
---
+++ frontend/src/components/games/Memory/index.tsx
@@ -0,0 +1,118 @@
+import { useState, useEffect } from 'react';
+
+import type { Card } from './memory.utils';
+import { generateSet } from './memory.utils';
+import useCounter from '../../../hooks/useCounter';
+import type { Score } from '../../../types/score';
+import Closure from '../../ui/Game/Closure';
+import ResultContainer from '../../ui/Game/ResultContainer';
+import ResultText from '../../ui/Game/ResultText';
+
+type GameProps = {
+  game: { name: string; color: string };
+  score: Score;
+  setScore: (score: Score) => void;
+};
+
+export default function Memory({ game, score, setScore }: GameProps) {
+  const [gameRunning, setGameRunning] = useState(true);
+  const { counter: turns, increment: incrementTurns, reset: resetTurns } = useCounter(0);
+  const [cards, setCards] = useState<Array<Card>>([]);
+  const { counter: picks, increment: incrementPicks, reset: resetPicks } = useCounter(0);
+  const { counter: pairs, increment: incrementPairs, reset: resetPairs } = useCounter(0);
+  const [targets, setTargets] = useState<Array<EventTarget> & Array<HTMLButtonElement>>([]);
+
+  async function startGame() {
+    const randomSet = await generateSet();
+    setCards([...randomSet]);
+  }
+
+  function handleClick(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    // Disables the button
+    e.currentTarget.disabled = true;
+    if (picks < 2) {
+      incrementPicks();
+      targets.push(e.currentTarget);
+      // Makes the card's front visible
+      e.currentTarget.style.opacity = '1';
+      if (picks === 1) {
+        incrementTurns();
+        const prevCard = targets[0];
+        const currentCard = targets[1];
+        if (prevCard.value === currentCard.value) {
+          // If a pair is found makes them visible and disables them
+          prevCard.style.opacity = '1';
+          prevCard.disabled = true;
+          e.currentTarget.disabled = true;
+          incrementPairs();
+        } else {
+          // Makes the card invisible after some time and makes the buttons active again
+          setTimeout(() => {
+            prevCard.style.opacity = '0';
+            currentCard.style.opacity = '0';
+          }, 350);
+          prevCard.disabled = false;
+          currentCard.disabled = false;
+        }
+        // Resets the targets array and picks
+        setTargets([]);
+        resetPicks();
+
+        // Checks if all the pairs have been found
+        if (pairs === cards.length / 2 - 1) {
+          if (turns < score.highestScore! || score.highestScore === -1) {
+            setScore({ highestScore: turns });
+          }
+          resetPairs();
+          setGameRunning(false);
+        }
+      }
+    }
+  }
+
+  useEffect(() => {
+    startGame();
+  }, [gameRunning]);
+
+  return (
+    <>
+      {gameRunning && (
+        <section className="grid">
+          {cards.map((card) => (
+            // eslint-disable-next-line react/jsx-key
+            <div
+              className="flex h-20 w-14 flex-col items-center justify-center rounded-lg border-4 border-[#7a7a7a] sm:h-24 sm:w-20"
+              style={{ backgroundColor: game.color }}
+            >
+              <button
+                className="h-full w-full bg-[#e7e6e6] text-3xl font-bold text-[#121212] opacity-0"
+                type="button"
+                onClick={(e) => {
+                  handleClick(e);
+                }}
+                value={card}
+              >
+                {card}
+              </button>
+            </div>
+          ))}
+        </section>
+      )}
+      {!gameRunning && (
+        <Closure
+          onClick={() => {
+            resetTurns();
+            setGameRunning(true);
+          }}
+          color={game.color}
+          score={score}
+        >
+          <ResultContainer>
+            <ResultText text="you win!" color={game.color} />
+            <p className="text-center text-4xl">{`Turns: ${turns - 1}`}</p>
+          </ResultContainer>
+        </Closure>
+      )}
+    </>
+  );
+}
